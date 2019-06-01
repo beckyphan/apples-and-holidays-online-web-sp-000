20df4077b08a8bd4a7c8f225351e83001f53efaa
Becky Phan <beckyphan@live.com>
2019-06-01 22:58:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,9 +95,8 @@ def all_supplies_in_holidays(holiday_hash)
         end
       else
         holiday_arr.each do |word|
-          holiday_cap = holiday_cap.strip + " #{word.capitalize!}"
+          holiday_cap = "#{word.capitalize!}"
         end
-        holiday_cap = "#{word.capitalize!}"
       end
       
       supply_array.each do |supply|
